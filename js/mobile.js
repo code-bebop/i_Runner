1a91e4960a186bb37342aa65b75dad8cd75ceb1c
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-06-04 17:44:10 +0900

    2020-06-04 17:41
    - 모바일 화면 퍼블리싱 완료
    - 스크롤 바 추가 완료
    - 사용자가 스크롤을 내리면 각 컨텐츠가 화면에 보이지 않다가 나타나는 animation 추가
    - header를 fixed시켜서 스크롤을 올리면 나타나고, 내리면 사라지는 기능 추가
    - top 버튼 추가

--- js/mobile.js
+++ js/mobile.js
@@ -1,13 +1,87 @@
+function topbtnHandler(scrollHeight) {
+  $(".main__btn--top").click((e)=>{
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop : 0
+    }, 1000);
+  })
+}
+
+function animationHandler(scrollHeight) {
+  if(scrollHeight > 3) {
+    $(".main__section--02__txt").addClass("play");
+  }
+  if (scrollHeight > 5){
+    $(".main__btn--top").addClass("on");
+  } else {
+    $(".main__btn--top").removeClass("on");
+  }
+  if(scrollHeight > 27) {
+    $(".main__section--03__content").addClass("play");
+    $(".main__section--03__background").addClass("play");
+  }
+  if(scrollHeight > 52) {
+    $(".main__section--04__copy").addClass("play");
+    $(".main__section--04__background").addClass("play");
+  }
+  if(scrollHeight > 73) {
+    $(".main__section--05__content").addClass("play");
+    $(".main__section--05__background").addClass("play");
+  }
+  
+  let parallax = (-scrollHeight + 30) * 2;
+  
+  $(".main__section--01__copy").css({
+    "transform": "translateX(" + (parallax - 60)*5 + "%)"
+  })
+  
+  $(".main__section--02__copy").css({
+    "transform": "translateX(" + parallax + "%)"
+  })
+}
+
+function scrollHandler() {
+  
+  const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  
+  $(".main__section--01__copy").addClass("play");
+  $(".main__section--01__background").addClass("play");
+  
+  $(window).scroll(()=>{
+    let currentHeight = windowHeight - document.documentElement.scrollTop;
+    let scrollHeight = 100 - ((currentHeight / windowHeight) * 100);
+    
+    $(".main__progress-bar__content").css("height", scrollHeight + "%");
+    
+    animationHandler(scrollHeight);
+  })
+}
+
 function menuHandler() {
   $(".header__top__btn").click((e)=>{
     e.preventDefault();
     $(".header__top__btn").toggleClass("is-opened");
     $(".header__product").toggleClass("is-opened");
   })
+  
+  let lastScrollTop = 0;
+  
+  $(window).scroll((e)=>{
+    let st = $(this).scrollTop();
+    
+    if (st > lastScrollTop){
+      $(".header").addClass("down");
+    } else {
+      $(".header").removeClass("down");
+    }
+    lastScrollTop = st;
+  })
 }
 
 function init() {
   menuHandler();
+  scrollHandler();
+  topbtnHandler();
 }
 
 init();
